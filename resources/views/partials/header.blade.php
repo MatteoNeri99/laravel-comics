9ef6bf9dd37e26780b279fde7f38c2e7e2a4eca1
MatteoNeri99 <matteolavoro99@gmail.com>
2024-07-15 19:41:38 +0200

    aggiunto partial header e img logo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<header>
+
+    <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+
+    <nav>
+      <ul>
+
+        <li>
+
+          <a href="#"> charaters</a>
+
+        </li>
+        <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+          <li>
+
+            <a href="#"> charaters</a>
+
+          </li>
+
+      </ul>
+
+    </nav>
+
+</header>
